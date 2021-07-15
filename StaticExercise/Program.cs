2e18d3f985930ea53354204e5a131c4c373f4e23
Garrett Breaux <garrett.breaux1@gmail.com>
2021-07-15 10:36:20 -0500

    Temp Converter

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{TempConverter.CelsiustoFahrenheit(111)}");
+            Console.WriteLine($"{TempConverter.FahrenheitToCelsius(79)}");
+        
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+        public static double FahrenheitToCelsius(double temp)
+        {
+            return ((temp - 32) *5 ) / 9;
+        }
+        public static double CelsiustoFahrenheit(double temp)
+        {
+            return ((temp * 9) / 5) + 32;
+        }
+    }
+}
